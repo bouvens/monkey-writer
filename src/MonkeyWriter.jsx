979f81feb8dde4ad2889e388001ae17c226794a5
Alexander Demin <bouvens@gmail.com>
2017-10-08 22:56:02 +0600

    Initial commit

---
+++ src/MonkeyWriter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class MonkeyWriter extends React.Component {
+    render () {
+        return (
+            <div>I'm a monkey!</div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import './index.css'
-
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
 
+import './index.css'
 import App from './App'
 
-render(<App/>, document.querySelector('#app'))
+render(<App />, document.querySelector('#app'))
